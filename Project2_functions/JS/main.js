10f0f9f554e6f0f78b67fc4af5a24e8225ecd8e1
Alison Liguori <alisonliguori0@gmail.com>
2025-11-03 17:09:22 -0500

    updated file

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  var sentence = "I am still learning";
+  sentence += "... and I still have a lot more to learn.";
+
+  // Update the paragraph with the new sentence
+  document.getElementById("test").innerHTML = sentence;
+}
